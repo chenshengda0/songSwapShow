7fa010770bfca7575de12a370419dff4986ffb38
shengdachen1991 <shengdachen1991@gmail.com>
2023-02-03 04:56:06 +0800

    add

--- dex-show/consumer.sh
+++ dex-show/consumer.sh
@@ -5,10 +5,12 @@ for (( i = 0; i < 60; i=(i+step) )); do
     #获取交易对最新区块高度
     curl http://127.0.0.1:9528/consumer/get_eth_price
 
+    sleep 5
+
     #消费获取交易对事件
     curl http://127.0.0.1:9528/consumer/get_btc_price
 
-}
+} &
     sleep $step
 
 done
