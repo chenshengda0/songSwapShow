5dc7b2c654d748f51831b9398d07ae1c886556fc
shengdachen1991 <shengdachen1991@gmail.com>
2023-02-03 05:22:48 +0800

    add

--- dex-show/consumer.sh
+++ dex-show/consumer.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-step=3 #间隔的秒数，不能大于60
+step=5 #间隔的秒数，不能大于60
 for (( i = 0; i < 60; i=(i+step) )); do
 {
     #获取交易对最新区块高度
-    curl http://127.0.0.1:9528/consumer/get_eth_price
-
-    sleep 5
-
-    #消费获取交易对事件
-    curl http://127.0.0.1:9528/consumer/get_btc_price
+    curl http://127.0.0.1:9528/consumer/get_coin_price
 
 } &
     sleep $step
